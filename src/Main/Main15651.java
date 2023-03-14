bdcf8101d1121b5d355ad590f5a888cbae9a53c3
ddongman <sky66951@naver.com>
2023-03-14 20:41:13 +0900

    백준 - 1722(조합, 아직 이해 못함), 백준 - 15649(백트래킹)

---
+++ src/Main/Main15651.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+public class Main15651 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer token;
+
+        token = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
+
+        // 1부터 N까지 자연수 중에서 M개를 고른 수열 (중복 가능)
+
+    }
+}
