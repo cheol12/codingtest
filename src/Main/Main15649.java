61ad5a2632dfc866740c5e53e507bb2b10b80aa5
cheol12 <sky66951@gmail.com>
2023-03-14 20:41:13 +0900

    백준 - 1722(조합, 아직 이해 못함), 백준 - 15649(백트래킹)

---
+++ src/Main/Main15649.java
@@ -0,0 +1,55 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+public class Main15649 {
+    static int N,M;
+    static boolean[] visited;
+    static int[] a;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token;
+        token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());    // 자연수 N개
+        M = Integer.parseInt(token.nextToken());    // N개 중 뽑을 M개
+
+        //1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+
+        visited = new boolean[N+1]; // 1~N 자연수 출력 여부 확인 배열
+        a = new int[M+1];           // M개만큼 뽑아서 출력할 배열
+
+        DFS(0);    // a[0]부터 출력하기 위해 DFS(0) 실행
+
+        bw.close();
+    }
+    static void DFS(int x) throws IOException{
+        // DFS를 통해 출력할 M개가 정해질 때마다 출력한다.
+        // = 동시에 하나의 DFS가 빠져나오는 조건
+        if(x == M){
+            for(int i=0; i<M; i++){ // M개만큼 출력해야함
+                bw.write(String.valueOf(a[i]) + " ");
+            }
+            bw.write("\n");
+            return;                 // 해당 DFS 빠져나오기
+        }
+        for(int i=1; i<=N; i++){    // 숫자 확인 후 a[i]에 위치시키는 DFS 연산
+            if(!visited[i]){        // 자연수 i가 출력에 쓰이지 않았다면
+                visited[i] = true;  // 출력하기로 한다.
+                a[x] = i;           // a[x], x번째 출력을 i로 한다.
+                //x += 1;           // 여기서 x+1을 하면 다음 DFS에서 빠져나올 때 증가된 채로 돌아와서 다음 반복문에 차질 생김.
+                DFS(x+1);           // 다음(=x+1번째) 출력값 구하기 위해 DFS(x+1)를 수행
+
+                // 다음 DFS에서 이전DFS로 빠져나올 때, i를 출력 안하는 것으로 다시 갱신
+                visited[i] = false;
+
+                // 이후로 계속 반복문 진행하며 출력할 수 있는 다른 수열 탐색
+            }
+        }
+    }
+}
+//https://www.youtube.com/watch?v=Enz2csssTCs
+//https://iseunghan.tistory.com/229
---
+++ src/Main/Main1722.java
@@ -0,0 +1,111 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+public class Main1722 {
+    //수학
+    //구현
+    //조합론
+    static int N;
+    static int[][] A;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer token;
+
+        //A = new int[fact(N)+1][N+1];    // 처음에는 모든 경우의 수를 구하려고 했음.
+
+        int Q;                      // 소문제 번호
+        long F[] = new long[21];    // F[i] = i! = 팩토리얼 값 나타내기
+        int S[] = new int[21];      // 출력할 K번째 순열
+        boolean visit[] = new boolean[21];  // 사용 숫자 나타내기
+
+        N = Integer.parseInt(br.readLine());    // 범위 : 1~20
+
+        token = new StringTokenizer(br.readLine());
+        Q = Integer.parseInt(token.nextToken());    // 소문제 번호 : 1 or 2
+
+        F[0] = 1;
+
+        for(int i=1; i<=N; i++){
+            F[i] = F[i - 1] * i;
+        }
+
+        // 경우의 수들을 적어 놓고 잘 관찰해보면 맨 앞자리 숫자에 따라 각각 6개의
+        // 경우가 있다는 것을 알 수 있다. 즉, 길이가 N인 순열에서 앞자리 숫자가 i개
+        // 결정되었을 때 각 숫자마다 (N-i)! 개의 경우가 나온다는 것이다.
+        //
+        // 소문제 Q = 1번일 때, k번째 순열을 나타내는 N개의 수 출력.
+        // ex) N = 4, Q = 1, K = 3
+        if(Q == 1){
+            long K = Long.parseLong(token.nextToken());
+
+            // 구하려는 순열의 i번째 위치.
+            for(int i = 1; i <= N; i++){    // i=1 / i=2
+
+                // j = 순열에 들어갈 숫자
+                // cnt = 확정되지 않은 수 중에서 cnt번째의 수
+                // 만약, S[1]=1로 확정된 상태에서 cnt=2라면,
+                // 이는 확정되지 않은 (2,3,4) 중 2번째인 3을 뜻한다.
+
+                // j=1, i번째 위치에 j가 들어가는지 판단하기. (->1번째에 1이 들어가는 것 확인)
+                // j=1, 1은 이미 순열에 들어감/ j=2, cnt=1일 때, K<=cnt*F[N-i]을 통해
+                // 확정되지 않은 1(cnt)번째 숫자인 2(j)는 2(i)번째에 들어갈 수 없는 것 확인
+                // j=2, cnt=2, 3<=2*2, 확정되지 않은 2번째 숫자인 3은
+                for(int j = 1, cnt = 1; j <= N; j++){
+
+                    // visit[1] = false->true / visit[2] = false
+                    if(visit[j]) continue;  // 이미 순열에 들어간 숫자는 건너뛰기.
+
+                    // K에 따라 각 자리에 들어갈 수 있는 수 찾기
+                    // i=1,j=1,cnt=1,K=3, 3 <= 1 * 6(성립)
+                    // i=2,j=2,cnt=1,K=3, 3 <= 1 * 2(성립X) >> cnt=2, 3<=2*2(성립)
+                    if(K <= cnt * F[N - i]){
+
+                        // K = 3 - 0*6 = 3
+                        // K = 3 - 1*2 = 3 - 2 = 1
+                        K -= ((cnt - 1) * F[N - i]);
+                        S[i] = j;   // S[1] = 1 / S[2] = 2
+                        visit[j] = true;    // visit[1] = true / visit
+                        break;
+                    }
+                    cnt++;
+                }
+            }
+            // S[1] = 1 => i=1, j=1, K -=
+            // S[2] = 3 =>
+            // S[3] = 2 =>
+            // S[4] = 4 =>
+            for(int i = 1; i <= N; i++){
+                bw.write(String.valueOf(S[i]) + " ");
+            }
+        }
+
+        // 소문제 = 2번일 때, N개의 수가 몇 번째 수열인지 출력(K로 출력해보기)
+        else if(Q == 2){
+            long K = 1;
+            for(int i=1; i<=N; i++){
+                S[i] = Integer.parseInt(token.nextToken());
+                long cnt = 0;
+                for(int j=1; j<S[i]; j++){
+                    if(visit[j] == false){
+                        cnt++;  // 미사용 숫자 개수만큼 카운트
+                    }
+
+                }
+                K += cnt * F[N-i];  // 자리수 개수에 따라 순서 더해주기
+                visit[S[i]] = true;
+            }
+            bw.write(String.valueOf(K));
+        }
+       bw.close();
+    }
+
+    // 팩토리얼 함수
+    static int fact(int a){
+        return a != 1 ? a * fact(a-1) : 1;
+    }
+
+}
+//https://it-earth.tistory.com/115
