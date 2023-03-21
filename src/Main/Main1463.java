7c61adbc8978f367d29fc6cf836375eff68abb06
ddongman <sky66951@naver.com>
2023-03-21 19:24:35 +0900

    백준 - 1010, 1256, 1463, 1722, 1947, 2193, 11726, 14501 (조합, 다이나믹 프로그래밍 등)

---
+++ src/Main/Main1463.java
@@ -0,0 +1,58 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+public class Main1463 {
+    // 다이나믹 프로그래밍
+    static int N;
+    static int[] D; // 연산 횟수 최소값을 저장하는 배열
+    static int count = 0;   // 연산 횟수
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+
+        D = new int[N+1];
+        // 초기화
+        D[1] = 0;
+
+        // 바텀-업 전개방식
+        // D[i] = i가 1이 될 때까지의 연산 횟수의 최솟값을 입력.
+        for(int i=2; i<=N; i++){
+            D[i] = D[i-1] + 1;  // 1을 빼는 연산
+
+            if(i % 2 == 0){
+                D[i] = Math.min(D[i], D[i/2] + 1);  // 2로 나누는 연산
+            }
+            if(i % 3 == 0){
+                D[i] = Math.min(D[i], D[i/3] + 1);  // 3으로 나누는 연산
+            }
+        }
+
+//        while(N != 1){
+//            if(N % 3 == 0){
+//                N /= 3;
+//                count++;
+//            }
+//            else if(N % 2 == 0){
+//                if((N - 1) % 3 == 0){
+//                    N = (N - 1) / 3;
+//                    count += 2;
+//                    continue;
+//                }
+//                N /= 2;
+//                count++;
+//            }
+//            else{
+//                N -= 1;
+//                count++;
+//            }
+//        }
+//        bw.write(String.valueOf(count));
+
+        bw.write(String.valueOf(D[N]));
+        bw.close();
+    }
+}
