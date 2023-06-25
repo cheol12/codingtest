72d4aee52859f410525228482c8ef14397dae731
cheol12 <sky66951@gmail.com>
2023-06-25 16:06:51 +0900

    백준 - 1932(정수 삼각형 주석 수정)

--- src/Main/Main1932.java
+++ src/Main/Main1932.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main1932 {
     static int n;
     static int[][] triangle;
-    static int[][] dp;
+//    static int[][] dp;
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,24 +15,29 @@ public class Main1932 {
 
         n = Integer.parseInt(br.readLine());
         triangle = new int[n+1][n+1];
-        dp = new int[n+1][n+1];
+//        dp = new int[n+1][n+1];
 
         for(int i=1; i<=n; i++){
             token = new StringTokenizer(br.readLine());
             for(int j=1; j<=i; j++){
                 triangle[i][j] = Integer.parseInt(token.nextToken());
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                triangle[i][j] = Math.max(triangle[i-1][j-1], triangle[i-1][j]) + triangle[i][j];
+//                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
             }
         }
 
-        int answer = 0;
+
+//        int answer = 0;
+        int answer2 = 0;
 
         for(int i=1; i<=n; i++){
-            answer = Math.max(answer, dp[n][i]);
+//            answer = Math.max(answer, dp[n][i]);
+            answer2 = Math.max(answer2, triangle[n][i]);
         }
 
 
-        bw.write(answer+"");
+//        bw.write(answer+"");
+        bw.write(answer2+"");
         bw.close();
     }
 
