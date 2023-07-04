67201b2b4aba5f790a5442e107f67ceb0fe6d447
cheol12 <sky66951@naver.com>
2023-07-04 21:55:11 +0900

    kb4기 과제 - EvenOddSum

--- src/CodeUp/Main.java
+++ src/CodeUp/Main.java
@@ -1,21 +1,57 @@
 package CodeUp;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N;
+    static int[][] arr;
+    static int result = 0;
+
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =
+                new BufferedReader(new FileReader("C:\\EvenOddSumInput.txt"));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        br.readLine();
-        String a = br.readLine();
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
 
-        String b[] = a.split("-");
-
-        for(int i=0; i<b.length; i++){
-            bw.write(b[i]);
+        StringTokenizer token;
+        int i = 0;
+        while(i<N){
+            token = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                arr[i][j] = Integer.parseInt(token.nextToken());
+            }
+            i++;
         }
+
+        BFS();
+
+        bw.write(result+"");
         bw.close();
+    }
+
+    static void BFS(){
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                int sum_temp = 0;
+
+                for(int k=0; k<4; k++){
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                    sum_temp += arr[nx][ny];
+                }
+                if(sum_temp % 2 == 0) result += arr[i][j];
+            }
+        }
     }
 }
