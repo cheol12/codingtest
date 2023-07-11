966421c6f44bf03bc6067f28eb0cc68201cbcf67
ddongman <cheol12>
2023-07-11 15:56:28 +0900

    백준 - 15650, 15651, 15652, 15654, 15655, 15656, 15657 (백트래킹 기본 문제, N과 M 시리즈)

---
+++ src/Main/Main15652.java
@@ -0,0 +1,40 @@
+package Main;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+
+    static int N,M;
+    static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token;
+
+        token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+        arr = new int[M+1];
+
+        DFS(1);
+        bw.close();
+    }
+
+    static void DFS(int i) throws IOException{
+        if(i == M + 1){
+            for(int j=1; j<=M; j++){
+                bw.write(arr[j] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int j=1; j<=N; j++){
+            if(!(arr[i-1] <= j)) continue;
+            arr[i] = j;
+            DFS(i + 1);
+        }
+    }
+}
