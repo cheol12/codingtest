39f29c2308027ba864e13d8d65704cd3bff43ffa
cheol12 <sky66951@gmail.com>
2023-12-04 16:50:16 +0900

    프로그래머스 - 전화번호목록(해시)

---
+++ src/programmers/Solution전화번호목록.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution전화번호목록 {
+    public static void main(String[] args){
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        System.out.println(solution(phone_book));
+    }
+
+    static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Map<String, Integer> hm = new HashMap<>();
+
+        for(String temp : phone_book){
+            hm.put(temp, 0);
+        }
+
+        for(String temp : phone_book){
+            for(int i=1; i<temp.length(); i++){
+                if(hm.containsKey(temp.substring(0, i)))
+                    answer = false;
+            }
+        }
+
+        return answer;
+    }
+}
