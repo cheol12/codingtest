fdb82a422c8b3f2051751334bdee881e024131d9
cheol12 <sky66951@gmail.com>
2023-12-04 18:02:22 +0900

    아직 안 푼 것

---
+++ src/Main/Main1744.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+class Main1744{
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+
+        }
+    }
+}
---
+++ src/Main/Main2457.java
@@ -0,0 +1,23 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+class Main2457{
+
+    static int N;
+    static int[][] date;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        // 1월 1일, 5월 31일
+        date = new int[N][4];
+        for(int i=0; i<N; i++){
+
+        }
+    }
+}
+
