30ea330f6618fbd0609f64609aca2202f69cc8ba
cheol12 <sky66951@gmail.com>
2023-12-08 16:47:35 +0900

    프로그래머스 - 올바른 괄호(스택/큐)

---
+++ src/programmers/Solution올바른괄호.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+public class Solution올바른괄호 {
+    public static void main(String[] args) {
+        String s = ")()(";
+        System.out.println(solution(s));
+    }
+    static boolean solution(String s) {
+        boolean answer = true;
+        long beforeTime = System.nanoTime(); //코드 실행 전에 시간 받아오기
+
+        //
+        Queue<Character> q = new ArrayDeque<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '(') q.offer('(');
+            else if(q.poll() == null) return false;
+        }
+
+        if(!q.isEmpty()) answer = false;
+        //
+
+        long afterTime = System.nanoTime(); // 코드 실행 후에 시간 받아오기
+        long secDiffTime = (afterTime - beforeTime) / 1000; // 실행 시간 계산
+        System.out.println("실행시간(ms) : "+secDiffTime);
+
+        return answer;
+    }
+}
