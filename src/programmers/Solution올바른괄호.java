2fd7c46101cd4a56ee563b725ad162c6402cfe96
cheol12 <sky66951@gmail.com>
2023-12-12 21:02:09 +0900

    프로그래머스 - 올바른 괄호(스택/큐) 큐 방식 + 스택 방식 추가

--- src/programmers/Solution올바른괄호.java
+++ src/programmers/Solution올바른괄호.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class Solution올바른괄호 {
     public static void main(String[] args) {
-        String s = ")()(";
+        String s = "(())()";
         System.out.println(solution(s));
     }
     static boolean solution(String s) {
         boolean answer = true;
         long beforeTime = System.nanoTime(); //코드 실행 전에 시간 받아오기
 
-        //
+        // 큐 방식 정답
         Queue<Character> q = new ArrayDeque<>();
 
         for(int i=0; i<s.length(); i++){
@@ -22,9 +22,22 @@ public class Solution올바른괄호 {
         if(!q.isEmpty()) answer = false;
         //
 
+        // 스택 방식 정답
+//        Stack<Character> stack = new Stack<>();
+//        try{
+//            for(int i=0; i<s.length(); i++){
+//                if(s.charAt(i) == '(') stack.push('(');
+//                else stack.pop();
+//            }
+//        }catch(Exception o){
+//            return false;
+//        }
+//        if(!stack.isEmpty()) answer = false;
+        //
+
         long afterTime = System.nanoTime(); // 코드 실행 후에 시간 받아오기
         long secDiffTime = (afterTime - beforeTime) / 1000; // 실행 시간 계산
-        System.out.println("실행시간(ms) : "+secDiffTime);
+        System.out.println("실행시간(ms) : " + secDiffTime);
 
         return answer;
     }
