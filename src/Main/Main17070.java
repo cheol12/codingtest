1f3289c8a109b80fba81897289b5c9704ff0dad9
cheol12 <sky66951@gmail.com>
2024-04-04 14:53:07 +0900

    백준 - 17070 파이프 옮기기 (DP, 그래프 탐색) - *주석 수정

--- src/Main/Main17070.java
+++ src/Main/Main17070.java
@@ -3,13 +3,12 @@ package Main;
 import java.io.*;
 import java.util.*;
 
-public class Main17070 {
-
+class Main17070{
     static int N;
     static int[][] house;
-    static int count = 0;
+    static int answer = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer token;
@@ -25,59 +24,46 @@ public class Main17070 {
             }
         }
 
-        // 1. 가로 라면
-        // 1-1. 오른쪽으로 밀 수 있는지
-        // 1-2. 대각선으로 꺾을 수 있는지
-
-        // 2. 대각선 이라면
-        // 2-1. 오른쪽으로 꺾을 수 있는지
-        // 2-2. 대각선으로 밀 수 있는지
-        // 2-3. 아래로 꺾을 수 있는지
-
-        // 3. 아래 라면
-        // 3-1. 대각선으로 꺾을 수 있는지
-        // 3-2. 아래로 밀 수 있는지
-
+        // 끝을 기준으로 시작
         DFS(1, 2, "right");
-
-        bw.write(count + "");
+        bw.write(answer + "");
         bw.close();
     }
 
-    static void DFS(int curR, int curC, String curDirection){
-        if(curR == N && curC == N){
-            count++;
+    static void DFS(int r, int c, String direction){
+        if(r == N && c == N) {
+            answer++;
             return;
         }
+        // 1. 방향 오른쪽일 때
+        //    1-1. 오른쪽 밀기
+        if(direction.equals("right")){
+            if(c + 1 <= N && house[r][c+1] == 0){
+                DFS(r, c + 1, "right");
+            }
+        }
+        // 2. 방향 대각선일 때
+        //    2-1. 오른쪽 꺾기
+        //    2-2. 아래쪽 밀기
+        else if(direction.equals("diagonal")){
+            if(c + 1 <= N && house[r][c+1] == 0){
+                DFS(r, c + 1, "right");
+            }
+            if(r + 1 <= N && house[r+1][c] == 0){
+                DFS(r + 1, c, "under");
+            }
+        }
+        // 3. 방향 아래일 때
+        //    3-1. 아래쪽 밀기
+        else if(direction.equals("under")){
+            if(r + 1 <= N && house[r+1][c] == 0){
+                DFS(r + 1, c, "under");
+            }
+        }
 
-        if(curDirection.equals("right")){
-            // 오른쪽으로 밀 수 있으면
-            if(curC + 1 <= N && house[curR][curC+1] == 0){
-                DFS(curR, curC + 1, "right");
-            }
-        }
-        else if(curDirection.equals("diagonal")){
-            // 오른쪽으로 꺾을 수 있으면
-            if(curC + 1 <= N && house[curR][curC+1] == 0){
-                DFS(curR, curC + 1, "right");
-            }
-            // 아래로 꺾을 수 있으면
-            if(curR + 1 <= N && house[curR+1][curC] == 0){
-                DFS(curR + 1, curC, "under");
-            }
-        }
-        else{
-            // 아래로 밀 수 있으면
-            if(curR + 1 <= N && house[curR+1][curC] == 0){
-                DFS(curR + 1, curC, "under");
-            }
-        }
-
-        // 대각선으로 밀 수 있으면 (방향 무관하게 공통 로직임)
-        if(curR + 1 <= N && curC + 1 <= N && house[curR][curC+1] == 0 &&
-                house[curR+1][curC+1] == 0 && house[curR+1][curC] == 0){
-            DFS(curR + 1, curC + 1, "diagonal");
+        // 방향 무관하게 공통 가능성 : 대각선 이동
+        if(r + 1 <= N && c + 1 <= N && house[r+1][c] == 0 && house[r][c+1] == 0 && house[r+1][c+1] == 0){
+            DFS(r + 1, c + 1, "diagonal");
         }
     }
-
 }
